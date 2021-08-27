a672b398421028565e1996677a0e4d23419f27a4
AbidFAli <ali.655@osu.edu>
2021-08-27 15:50:24 -0400

    Added eslint

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -1,79 +1,73 @@
-import axios from 'axios';
-import { isTokenExpiredError } from '../util/errors';
-import { userService } from './userService';
+import axios from "axios";
+import { isTokenExpiredError } from "../util/errors";
+import { userService } from "./userService";
 
-const BASE_URL = 'http://localhost:3001/api/recipes'
-const MESSAGE_DELETE_SUCCESS = "deletion successful"
-const MESSAGE_DELETE_FAIL = "deletion failed"
+const BASE_URL = "http://localhost:3001/api/recipes";
 const DEFAULT_SEARCH_BATCH_SIZE = 20;
 
 const authHeaderForUser = (user) => {
-  return {'Authorization': `Bearer ${user.token}`}
-}
-
-
-
+  return { Authorization: `Bearer ${user.token}` };
+};
 
 //will throw TokenExpiredErrors
 const create = async (recipe, user) => {
-  try{
-    const result = await axios.post(BASE_URL + '/', recipe, {headers: authHeaderForUser(user)});
+  try {
+    const result = await axios.post(BASE_URL + "/", recipe, {
+      headers: authHeaderForUser(user),
+    });
     return result.data;
-  }catch(error){
-    console.log(error)
-    if(isTokenExpiredError(error)){
-      throw error
+  } catch (error) {
+    console.log(error);
+    if (isTokenExpiredError(error)) {
+      throw error;
     }
   }
-}
-
+};
 
 const getById = async (id) => {
-  try{
+  try {
     const result = await axios.get(BASE_URL + `/${id}`);
     return result.data;
-  }catch(error){
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
 const getAll = async () => {
-  try{
-    const result = await axios.get(BASE_URL + '/');
+  try {
+    const result = await axios.get(BASE_URL + "/");
     return result.data;
-  }catch(error){
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
 //checks for errors in the search parameters
 const processSearchParams = (params) => {
-    let paramsToSend = {...params}
+  let paramsToSend = { ...params };
 
-    if(typeof(params.name)  === 'string'){
-      paramsToSend.name = params.name.trim()
-    }
-    else{
-      throw Error("params.name must be string")
-    }
+  if (typeof params.name === "string") {
+    paramsToSend.name = params.name.trim();
+  } else {
+    throw Error("params.name must be string");
+  }
 
-    if(typeof(params.ingredient)  === 'string'){
-      paramsToSend.ingredient = params.ingredient.trim()
-    }
-    else{
-      throw Error("params.ingredient must be string")
-    }
+  if (typeof params.ingredient === "string") {
+    paramsToSend.ingredient = params.ingredient.trim();
+  } else {
+    throw Error("params.ingredient must be string");
+  }
 
-    if(typeof(params.ratingMin) !== 'number' || Number.isNaN(params.ratingMin)){
-      throw Error("params.ratingMin must be a number")
-    }
+  if (typeof params.ratingMin !== "number" || Number.isNaN(params.ratingMin)) {
+    throw Error("params.ratingMin must be a number");
+  }
 
-    if(typeof(params.ratingMax) !== 'number' || Number.isNaN(params.ratingMin)){
-      throw Error("params.ratingMax must be a number")
-    }
-    
-    return paramsToSend
-}
+  if (typeof params.ratingMax !== "number" || Number.isNaN(params.ratingMin)) {
+    throw Error("params.ratingMax must be a number");
+  }
+
+  return paramsToSend;
+};
 
 /*
 *@param start: Number; the position(starts at 0) of the recipe in the search results from which to begin returning.
@@ -86,64 +80,65 @@ const processSearchParams = (params) => {
 * count: Number; whether to return recipes or a count of the number of recipes matching the search criteria;
 *}
 */
-const search = async (params, start, size = DEFAULT_SEARCH_BATCH_SIZE ) => {
-  let paramsToSend = processSearchParams(params)
-  if(!paramsToSend.count){
-    paramsToSend.size = size
-    paramsToSend.start = start
+const search = async (params, start, size = DEFAULT_SEARCH_BATCH_SIZE) => {
+  let paramsToSend = processSearchParams(params);
+  if (!paramsToSend.count) {
+    paramsToSend.size = size;
+    paramsToSend.start = start;
   }
-  let results = await axios.get(BASE_URL + '/search', {
-    params: paramsToSend
-  })
-  return results.data
-}
+  let results = await axios.get(BASE_URL + "/search", {
+    params: paramsToSend,
+  });
+  return results.data;
+};
 
 const getNumberSearchResults = async (params) => {
-  let paramsToSend = {...params}
-  paramsToSend.count = 'true'
-  let results = await search(paramsToSend)
-  return results.count
-}
+  let paramsToSend = { ...params };
+  paramsToSend.count = "true";
+  let results = await search(paramsToSend);
+  return results.count;
+};
 
 //will throw TokenExpiredErrors
 const update = async (recipe, user) => {
-  try{
-    const result = await axios.put(BASE_URL + `/${recipe.id}`, recipe, {headers: authHeaderForUser(user)});
+  try {
+    const result = await axios.put(BASE_URL + `/${recipe.id}`, recipe, {
+      headers: authHeaderForUser(user),
+    });
     return result.data;
-  }
-  catch(error){
-    console.log(error)
-    if(isTokenExpiredError(error)){
-      throw error
+  } catch (error) {
+    console.log(error);
+    if (isTokenExpiredError(error)) {
+      throw error;
     }
   }
-}
+};
 
 //will throw TokenExpiredErrors
 const destroy = async (recipeId, user) => {
-  try{
-    await axios.delete(BASE_URL + `/${recipeId}`, {headers: authHeaderForUser(user)})
-  }
-  catch(error){
-    console.log(error)
-    if(isTokenExpiredError(error)){
-      throw error
+  try {
+    await axios.delete(BASE_URL + `/${recipeId}`, {
+      headers: authHeaderForUser(user),
+    });
+  } catch (error) {
+    console.log(error);
+    if (isTokenExpiredError(error)) {
+      throw error;
     }
   }
-}
+};
 
 /*
-*@returns [{name, id}]
-*/
+ *@returns [{name, id}]
+ */
 const getRecipesForUser = async (userId) => {
-  try{
-    const user = await userService.getById(userId)
-    return user.recipes
+  try {
+    const user = await userService.getById(userId);
+    return user.recipes;
+  } catch (error) {
+    console.log(error);
   }
-  catch(error){
-    console.log(error)
-  }
-}
+};
 
 const recipeService = {
   create,
@@ -154,8 +149,7 @@ const recipeService = {
   getRecipesForUser,
   search,
   getNumberSearchResults,
-  DEFAULT_SEARCH_BATCH_SIZE
-}
+  DEFAULT_SEARCH_BATCH_SIZE,
+};
 
 export default recipeService;
-
